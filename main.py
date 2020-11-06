69070a346825c047340f368560fc55733bb7d39a
mranch <anchiforov@gmail.com>
2020-11-06 15:27:28 +0200

    Added example of inline handler (to use @EatHealthyBot <any text> to convert it to uppercase in any chat)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
 import logging
 import os
 
@@ -20,10 +21,34 @@ def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = [InlineQueryResultArticle(
+        id=query.upper(),
+        title=query.upper(),
+        input_message_content=InputTextMessageContent(query.upper())
+    )]
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
 
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
 updater.start_polling()
+updater.idle()
